4fa4c1d56adc1005ce856580848a11bc11598e09
Michael Carpenter <malcom2073@gmail.com>
2013-09-16 18:21:20 -0400

    Change for alpha-RC1 version name

--- src/configuration.h
+++ src/configuration.h
@@ -11,7 +11,7 @@
 #define WITH_TEXT_TO_SPEECH 1
 
 #define QGC_APPLICATION_NAME "APM Planner"
-#define QGC_APPLICATION_VERSION "v2.0.0 (beta)"
+#define QGC_APPLICATION_VERSION "v2.0.0 (alpha-RC1)"
 
 namespace QGC
 
