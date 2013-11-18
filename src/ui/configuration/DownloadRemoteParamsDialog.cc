56fb8838d441488144e40e778f63268bfb301363
Bill Bonney <billbonney@communistech.com>
2013-11-18 09:15:03 -0800

    Fixes issue if directory for parameters does not exist

--- src/ui/configuration/DownloadRemoteParamsDialog.cc
+++ src/ui/configuration/DownloadRemoteParamsDialog.cc
@@ -128,22 +128,14 @@ void DownloadRemoteParamsDialog::startFileDownloadRequest(QUrl url)
 
 void DownloadRemoteParamsDialog::downloadParamFile()
 {
-#ifdef Q_OS_WIN
-    QString appDataDir = QString(getenv("USERPROFILE")).replace("\\","/");
-#else
-    QString appDataDir = getenv("HOME");
-#endif
+    QString homeDir = QDesktopServices::storageLocation(QDesktopServices::HomeLocation);
 
-    QDir appDir(appDataDir);
+    QDir parameterDir = QDir(homeDir + "/apmplanner2/parameters");
 
-    if (appDir.exists()) {
-        if (!appDir.cd("apmplanner2/parameters"))
-        {
-            appDir.mkdir("apmplanner2/parameters");
-        }
-    }
+    if (!parameterDir.exists())
+        parameterDir.mkdir(parameterDir.filePath(""));
 
-    QString fileName(QDir(appDataDir + "/apmplanner2/parameters").filePath(
+    QString fileName(parameterDir.filePath(
                           ui->listWidget->currentItem()->text()));
 
     if (fileName.isEmpty())
