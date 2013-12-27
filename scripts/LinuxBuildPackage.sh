b2e0a4cd3013a5d2457c9fc8b735683421845c35
stephendade <stephen_dade@hotmail.com>
2013-12-27 09:21:32 +0800

    qmake install now works in Linux

--- scripts/LinuxBuildPackage.sh
+++ scripts/LinuxBuildPackage.sh
@@ -1,60 +1,30 @@
 #!/bin/bash
 
 #run inside the /Script dir
-cd ../../
+cd ../
+
+NOW=$(date +"%Y%m%d")
 
 #build APM Planner
-qmake-qt4 ./apm_planner/qgroundcontrol.pro
+qmake-qt4 PREFIX=~/Documents/APMPlanner-$NOW/usr qgroundcontrol.pro
 make --jobs=3
+make install
 
 #Create folder structure
-NOW=$(date +"%Y%m%d")
-mkdir apmplanner-$NOW
-mkdir ./apmplanner-$NOW/debian
-mkdir ./apmplanner-$NOW/usr
-mkdir ./apmplanner-$NOW/usr/bin
-mkdir -p ./apmplanner-$NOW/usr/share/apmplanner2
-mkdir -p ./apmplanner-$NOW/usr/share/doc/apmplanner2
-mkdir -p ./apmplanner-$NOW/usr/share/menu
-mkdir -p ./apmplanner-$NOW/usr/share/applications
-
-#copy files over
-cp -r -f ./release/apmplanner2 ./apmplanner-$NOW/usr/bin
-cp -r -f ./release/data ./apmplanner-$NOW/usr/share/apmplanner2/data
-cp -r -f ./release/files ./apmplanner-$NOW/usr/share/apmplanner2/files
-cp -r -f ./release/qml ./apmplanner-$NOW/usr/share/apmplanner2/qml
+mkdir ~/Documents/APMPlanner-$NOW/DEBIAN
+install -m 755 -d ~/Documents/APMPlanner-$NOW/usr/share/doc/APMPlanner
 
 #copy deb support files
-cp -r -f ./apm_planner/scripts/debian/control ./apmplanner-$NOW/debian/control
-cp -f ./apm_planner/license.txt ./apmplanner-$NOW/usr/share/doc/apmplanner2/copyright
-cp -f ./apm_planner/scripts/debian/changelog ./apmplanner-$NOW/usr/share/doc/apmplanner2/changelog
-cp -r -f ./apm_planner/scripts/debian/postinst ./apmplanner-$NOW/debian/postinst
-cp -r -f ./apm_planner/scripts/debian/postrm ./apmplanner-$NOW/debian/postrm
-cp -f ./apm_planner/scripts/debian/apmplanner2 ./apmplanner-$NOW/usr/share/menu/apmplanner2
-cp -f ./apm_planner/scripts/debian/apmplanner2.desktop ./apmplanner-$NOW/usr/share/applications/apmplanner2.desktop
-gzip -9 ./apmplanner-$NOW/usr/share/doc/apmplanner2/changelog
-
-#make symbolic links to folders
-cd ./apmplanner-$NOW/usr/bin
-ln -s -r ../share/apmplanner2/files
-ln -s -r ../share/apmplanner2/data
-ln -s -r ../share/apmplanner2/qml
-cd ../../../
-
-#strip symbols from the binary
-strip -s  ./apmplanner-$NOW/usr/bin/apmplanner2
-mv ./apmplanner-$NOW/debian ./apmplanner-$NOW/DEBIAN
-
-#fix up permissions
-find ./apmplanner-$NOW/usr -type d -exec chmod 755 {} \;
-find ./apmplanner-$NOW/usr -type f -exec chmod 644 {} \;
-chmod 755 ./apmplanner-$NOW/DEBIAN/postinst
-chmod 755 ./apmplanner-$NOW/DEBIAN/postrm
-chmod +x ./apmplanner-$NOW/usr/bin/apmplanner2
+cp -r -f ./scripts/debian/control ~/Documents/APMPlanner-$NOW/DEBIAN/control
+install -m 644 ./license.txt ~/Documents/APMPlanner-$NOW/usr/share/doc/APMPlanner/copyright
+install -m 644 ./scripts/debian/changelog ~/Documents/APMPlanner-$NOW/usr/share/doc/APMPlanner/changelog
+install -m 755 ./scripts/debian/postinst ~/Documents/APMPlanner-$NOW/DEBIAN/postinst
+install -m 755 ./scripts/debian/postrm ~/Documents/APMPlanner-$NOW/DEBIAN/postrm
+gzip -9 ~/Documents/APMPlanner-$NOW/usr/share/doc/APMPlanner/changelog
 
 #create the pacakge and check compliance (report.txt)
-fakeroot dpkg-deb -b ./apmplanner-$NOW
-lintian apmplanner-$NOW.deb > report.txt
+fakeroot dpkg-deb -b ~/Documents/APMPlanner-$NOW
+lintian ~/Documents/APMPlanner-$NOW.deb > ~/Documents/report.txt
 
 
 
--- scripts/LinuxBuildPackageReadme.txt
+++ scripts/LinuxBuildPackageReadme.txt
@@ -1,9 +1,9 @@
 ---Using the Linux Script---
 
 The LinuxBuildPackage.sh script will do the following things:
-1. Build a release of apm_planner in the directory above apm_planner
-2. Create a .deb debian package in the directory above apm_planner
-3. Run the lintian checker to check for any errors in the package. A report is output at report.txt
+1. Build a release of apm_planner in the ~/Documents directory
+2. Create a .deb debian package in the ~/Documents directory
+3. Run the lintian checker to check for any errors in the package. A report is output at ~/Documents/report.txt
 
 Using the debian package:
 Due to this package not being an "official" Ubuntu/Debian/etc package, the installation process is a little convoluted for the first install. This is due to any dependencies not being able to be automatically installed alongside APM Planner.
