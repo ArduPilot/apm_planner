cde7392f31bc2760b657c2db1e880492bb457529
stephendade <stephen_dade@hotmail.com>
2013-12-27 09:52:13 +0800

    qmake install now working under Linux

--- scripts/LinuxBuildPackage.sh
+++ scripts/LinuxBuildPackage.sh
@@ -1,60 +1,30 @@
 #!/bin/bash
 
 #run inside the /Script dir
-cd ../../
+cd ../
+
+NOW=$(date +"%Y%m%d")
 
 #build APM Planner
-qmake-qt4 ./apm_planner/qgroundcontrol.pro
+qmake-qt4 PREFIX=~/Documents/APMPlanner2-$NOW/usr qgroundcontrol.pro
 make --jobs=3
+make install
 
 #Create folder structure
-NOW=$(date +"%Y%m%d")
-mkdir apmplanner-$NOW
-mkdir ./apmplanner-$NOW/debian
-mkdir ./apmplanner-$NOW/usr
-mkdir ./apmplanner-$NOW/usr/bin
-mkdir -p ./apmplanner-$NOW/usr/share/apmplanner2
-mkdir -p ./apmplanner-$NOW/usr/share/doc/apmplanner2
-mkdir -p ./apmplanner-$NOW/usr/share/menu
-mkdir -p ./apmplanner-$NOW/usr/share/applications
-
-#copy files over
-cp -r -f ./release/apmplanner2 ./apmplanner-$NOW/usr/bin
-cp -r -f ./release/data ./apmplanner-$NOW/usr/share/apmplanner2/data
-cp -r -f ./release/files ./apmplanner-$NOW/usr/share/apmplanner2/files
-cp -r -f ./release/qml ./apmplanner-$NOW/usr/share/apmplanner2/qml
+mkdir ~/Documents/APMPlanner2-$NOW/DEBIAN
+install -m 755 -d ~/Documents/APMPlanner2-$NOW/usr/share/doc/APMPlanner2
 
 #copy deb support files
-cp -r -f ./apm_planner/scripts/debian/control ./apmplanner-$NOW/debian/control
-cp -f ./apm_planner/license.txt ./apmplanner-$NOW/usr/share/doc/apmplanner2/copyright
-cp -f ./apm_planner/scripts/debian/changelog ./apmplanner-$NOW/usr/share/doc/apmplanner2/changelog
-cp -r -f ./apm_planner/scripts/debian/postinst ./apmplanner-$NOW/debian/postinst
-cp -r -f ./apm_planner/scripts/debian/postrm ./apmplanner-$NOW/debian/postrm
-cp -f ./apm_planner/scripts/debian/apmplanner2 ./apmplanner-$NOW/usr/share/menu/apmplanner2
-cp -f ./apm_planner/scripts/debian/apmplanner2.desktop ./apmplanner-$NOW/usr/share/applications/apmplanner2.desktop
-gzip -9 ./apmplanner-$NOW/usr/share/doc/apmplanner2/changelog
-
-#make symbolic links to folders
-cd ./apmplanner-$NOW/usr/bin
-ln -s -r ../share/apmplanner2/files
-ln -s -r ../share/apmplanner2/data
-ln -s -r ../share/apmplanner2/qml
-cd ../../../
-
-#strip symbols from the binary
-strip -s  ./apmplanner-$NOW/usr/bin/apmplanner2
-mv ./apmplanner-$NOW/debian ./apmplanner-$NOW/DEBIAN
-
-#fix up permissions
-find ./apmplanner-$NOW/usr -type d -exec chmod 755 {} \;
-find ./apmplanner-$NOW/usr -type f -exec chmod 644 {} \;
-chmod 755 ./apmplanner-$NOW/DEBIAN/postinst
-chmod 755 ./apmplanner-$NOW/DEBIAN/postrm
-chmod +x ./apmplanner-$NOW/usr/bin/apmplanner2
+cp -r -f ./scripts/debian/control ~/Documents/APMPlanner2-$NOW/DEBIAN/control
+install -m 644 ./license.txt ~/Documents/APMPlanner2-$NOW/usr/share/doc/APMPlanner2/copyright
+install -m 644 ./scripts/debian/changelog ~/Documents/APMPlanner2-$NOW/usr/share/doc/APMPlanner2/changelog
+install -m 755 ./scripts/debian/postinst ~/Documents/APMPlanner2-$NOW/DEBIAN/postinst
+install -m 755 ./scripts/debian/postrm ~/Documents/APMPlanner2-$NOW/DEBIAN/postrm
+gzip -9 ~/Documents/APMPlanner2-$NOW/usr/share/doc/APMPlanner2/changelog
 
 #create the pacakge and check compliance (report.txt)
-fakeroot dpkg-deb -b ./apmplanner-$NOW
-lintian apmplanner-$NOW.deb > report.txt
+fakeroot dpkg-deb -b ~/Documents/APMPlanner2-$NOW
+lintian ~/Documents/APMPlanner2-$NOW.deb > ~/Documents/report.txt
 
 
 
