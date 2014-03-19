8c728348c37286cd02e5402de9e3236516002ee5
Bill Bonney <billbonney@communistech.com>
2014-03-19 15:39:33 -0700

    Update build scripts to label builds with version numbers of our releases. This will allow easy rollback for users to an older build as they can be more easily identified.

--- scripts/upload_osx.sh
+++ scripts/upload_osx.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-scp -P2270 -v apm_planner2_$BUILD_ID.dmg  user@somewhere.com:target_dir
+#!/bin/sh
+COMMIT_SHA=$(git describe --dirty)
+TARGET_DIR=/Users/Shared/Jenkins/Home/data/APMPlanner
+WEBSITE_USER=
+
+mv apm_planner2_$BUILD_ID\_osx.dmg ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg 
+cp ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg ${TARGET_DIR}/apm_planner2_latest_osx.dmg
+rsync -avh --password-file=rsyncpass ${TARGET_DIR} ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/
