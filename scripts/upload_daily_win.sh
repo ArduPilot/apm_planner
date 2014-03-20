680e33bfd65854ce790aafcd82706514469c1d51
Michael Carpenter <malcom2073@gmail.com>
2014-03-20 19:19:46 -0400

    Change so windows uploader script renames the file properly

--- scripts/upload_daily_win.sh
+++ scripts/upload_daily_win.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Move and Upload
-COMMIT_SHA=$(git rev-parse HEAD)
+COMMIT_SHA=$(git describe --dirty)
 TARGET_DIR=/var/www/wordpress/APMPlanner2/daily
 TARGET_SUBDIR=${BUILD_ID:0:10}
 
@@ -8,7 +8,7 @@ if [ ! -d ${TARGET_DIR}/${TARGET_SUBDIR} ]; then
   mkdir ${TARGET_DIR}/${TARGET_SUBDIR}/
 fi
 
-mv apmplanner2-installer-win32.exe ${TARGET_DIR}/${TARGET_SUBDIR}/apm_planner2_${COMMIT_SHA:0:8}_win.exe
+mv apmplanner2-installer-win32.exe ${TARGET_DIR}/${TARGET_SUBDIR}/apm_planner_${COMMIT_SHA}_win.exe
 
 #This will eventually contain my rsync line...
 rsync -avh --password-file=${RSYNC_PASSFILE} ${TARGET_DIR}/${TARGET_SUBDIR} ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/daily/
