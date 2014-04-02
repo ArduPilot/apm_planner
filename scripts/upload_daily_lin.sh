09cd3c53ddcc6c1b30a3dcd0c1fe958167806fb1
Michael Carpenter <malcom2073@gmail.com>
2014-04-02 13:15:04 -0400

    Change to support new linux installer naming scheme

--- scripts/upload_daily_lin.sh
+++ scripts/upload_daily_lin.sh
@@ -8,10 +8,10 @@ if [ ! -d ${TARGET_DIR}/${TARGET_SUBDIR} ]; then
   mkdir ${TARGET_DIR}/${TARGET_SUBDIR}/
 fi
 
-mv apm_planner_${BUILD_TAG}_wheezy.i386.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
-mv apm_planner_${BUILD_TAG}_wheezy.amd64.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
-mv apm_planner_${BUILD_TAG}_raring.i386.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
-mv apm_planner_${BUILD_TAG}_raring.amd64.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
+mv apm_planner_${BUILD_TAG}_debian.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
+mv apm_planner_${BUILD_TAG}_debian64.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
+mv apm_planner_${BUILD_TAG}_ubuntu.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
+mv apm_planner_${BUILD_TAG}_ubuntu64.deb ${TARGET_DIR}/${TARGET_SUBDIR}/
 
 #This will eventually contain my rsync line...
 rsync -avh --password-file=${RSYNC_PASSFILE} ${TARGET_DIR}/${TARGET_SUBDIR} ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/daily/
