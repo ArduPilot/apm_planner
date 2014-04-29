4ed97bf134eeac749fca6ae2447bb6ebf1ba371a
Bill Bonney <billbonney@communistech.com>
2014-04-28 17:45:26 -0700

    Basic Tuning: Add RC Feel as a slider to adjust.

--- src/ui/configuration/BasicPidConfig.cc
+++ src/ui/configuration/BasicPidConfig.cc
@@ -32,21 +32,29 @@ BasicPidConfig::BasicPidConfig(QWidget *parent) : AP2ConfigWidget(parent)
     ui.verticalLayout->insertWidget(0,m_rollPitchRateWidget);
     connect(m_rollPitchRateWidget,SIGNAL(doubleValueChanged(QString,double)),this,SLOT(rPRCValueChanged(QString,double)));
     m_rollPitchRateWidget->setupDouble(QString("Roll/Pitch Rate Control"),
-                                       "Slide to the right if the copter is sluggish or slide to the left if the copter is twitchy",0.15,0.08,0.4,0.01);
+                                       "Slide to the right if the copter is sluggish or slide to the left if the copter is twitchy.",0.15,0.08,0.4,0.01);
     m_rollPitchRateWidget->show();
 
+    m_rcFeelWidget = new ParamWidget("RcFeelControl",this);
+    ui.verticalLayout->insertWidget(1,m_rcFeelWidget);
+    connect(m_rcFeelWidget,SIGNAL(intValueChanged(QString,int)),this,SLOT(rcFeelValueChanged(QString,int)));
+    m_rcFeelWidget->setupInt(QString("RC Feel"),
+        tr("RC feel for roll/pitch which controls vehicle response to user input with 0 being extremely soft and 100 being crisp.\nVery Soft=0   Soft=25   Medium=50   Crisp=75   Very Crisp=100")
+                                       ,50,0,100);
+    m_rcFeelWidget->show();
+
     m_throttleHoverWidget = new ParamWidget("ThrottleHover",this);
-    ui.verticalLayout->insertWidget(1,m_throttleHoverWidget);
+    ui.verticalLayout->insertWidget(2,m_throttleHoverWidget);
     connect(m_throttleHoverWidget,SIGNAL(intValueChanged(QString,int)),this,SLOT(tHValueChanged(QString,int)));
     m_throttleHoverWidget->setupInt(QString("Throttle Hover "),
-                                    "How much throttle is needed to maintain a steady hover",480,300,700);
+                                    "How much throttle is needed to maintain a steady hover.",480,300,700);
     m_throttleHoverWidget->show();
 
     m_throttleAccelWidget = new ParamWidget("ThrottleAccel",this);
-    ui.verticalLayout->insertWidget(2,m_throttleAccelWidget);
+    ui.verticalLayout->insertWidget(3,m_throttleAccelWidget);
     connect(m_throttleAccelWidget,SIGNAL(doubleValueChanged(QString,double)),this,SLOT(tAValueChanged(QString,double)));
     m_throttleAccelWidget->setupDouble(QString("Throttle Accel"),
-                                       "Slide to the right to climb more aggressively or slide to the left to climb more gently",0.75,0.3,1.0,0.05);
+                                       "Slide to the right to climb more aggressively or slide to the left to climb more gently.",0.75,0.3,1.0,0.05);
     m_throttleAccelWidget->show();
 
     initConnections();
@@ -92,6 +100,18 @@ void BasicPidConfig::tHValueChanged(QString name,int value)
     m_uas->getParamManager()->setParameter(1,"THR_MID",value);
 }
 
+void BasicPidConfig::rcFeelValueChanged(QString name, int value)
+{
+    Q_UNUSED(name);
+
+    if (!m_uas)
+    {
+        showNullMAVErrorMessageBox();
+        return;
+    }
+    m_uas->getParamManager()->setParameter(1,"RC_FEEL_RP",value);
+}
+
 BasicPidConfig::~BasicPidConfig()
 {
 }
@@ -118,7 +138,12 @@ void BasicPidConfig::parameterChanged(int uas, int component, QString parameterN
     }
     else if (parameterName == "THR_MID")
     {
-         QLOG_DEBUG() << "BasicPID: THR_MID:" << value.toDouble();
+         QLOG_DEBUG() << "BasicPID: THR_MID:" << value.toInt();
         m_throttleHoverWidget->setValue(value.toInt());
+    }
+    else if (parameterName == "RC_FEEL_RP")
+    {
+        QLOG_DEBUG() << "BasicPID: RC_FEEL:" << value.toInt();
+        m_rcFeelWidget->setValue(value.toInt());
     }
 }
