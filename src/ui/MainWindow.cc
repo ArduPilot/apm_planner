ffe502ed81b9e2dc23d4365338b49c0fce2deccc
Michael Carpenter <malcom2073@gmail.com>
2014-06-18 16:26:55 -0400

    Comms: Fixed mavlink message typo, and reenabled MAVLink Inspector

--- src/ui/MainWindow.cc
+++ src/ui/MainWindow.cc
@@ -888,7 +888,7 @@ void MainWindow::loadDockWidget(QString name)
     }
     else if (name == "MAVLINK_INSPECTOR_DOCKWIDGET")
     {
-        /*QGCMAVLinkInspector *widget = new QGCMAVLinkInspector(mavlink,this);
+        QGCMAVLinkInspector *widget = new QGCMAVLinkInspector(0,this);
         logPlayer->setMavlinkInspector(widget);
         createDockWidget(centerStack->currentWidget(),widget,tr("MAVLink Inspector"),"MAVLINK_INSPECTOR_DOCKWIDGET",currentView,Qt::RightDockWidgetArea);
         //logPlayer*/
