4768aef1d0a897ca566dd4df01623be1ed0e7de3
Andre Kjellstrup <andre.kjellstrup@gmail.com>
2014-08-25 14:08:35 +0200

    Mission DoChangeSpeed updated
    Added extra parameters.

--- src/ui/mission/QGCMissionDoChangeSpeed.cc
+++ src/ui/mission/QGCMissionDoChangeSpeed.cc
@@ -10,9 +10,13 @@ QGCMissionDoChangeSpeed::QGCMissionDoChangeSpeed(WaypointEditableView* WEV) :
     this->WEV = WEV;
     //Using UI to change WP:
     connect(this->ui->param1SpinBox, SIGNAL(valueChanged(double)),WEV,SLOT(changedParam1(double)));
+    connect(this->ui->param2SpinBox, SIGNAL(valueChanged(double)),WEV,SLOT(changedParam2(double)));
+    connect(this->ui->param3SpinBox, SIGNAL(valueChanged(double)),WEV,SLOT(changedParam3(double)));
 
     //Reading WP to update UI:
     connect(WEV,SIGNAL(param1Broadcast(double)),this->ui->param1SpinBox,SLOT(setValue(double)));
+    connect(WEV,SIGNAL(param2Broadcast(double)),this->ui->param2SpinBox,SLOT(setValue(double)));
+    connect(WEV,SIGNAL(param3Broadcast(double)),this->ui->param3SpinBox,SLOT(setValue(double)));
 }
 
 QGCMissionDoChangeSpeed::~QGCMissionDoChangeSpeed()
