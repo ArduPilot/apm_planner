74cf4c72dba41ea5efcc9dc21f89626460bef6ba
Bill Bonney <billbonney@communistech.com>
2014-10-22 13:09:50 -0700

    Scripts: Update for Jenkins Builds on OSX

--- scripts/qt4to5.sh
+++ scripts/qt4to5.sh
@@ -2,15 +2,17 @@
 if [ $# -eq 0 ]
 then
 	QT_VERSION="5.2.1"
-	QT_PATH=${QT_VERSION}/${QT_VERSION}
+	QT_PATH=Qt${QT_VERSION}/${QT_VERSION}
+	BASE_DIR="/Developer/"
 else
 	QT_VERSION=$1
-	QT_PATH=${QT_VERSION}/${QT_VERSION:0:3}
+	QT_PATH=Qt${1}/${1:0:3}
+	BASE_DIR="/Developer/"
 fi
 
 rm /usr/bin/qmake
-echo "Creating a new link to" ${QT_PATH}
-if ln -s ${HOME}/QT_PATH/clang_64/bin/qmake /usr/bin/qmake
+echo "Creating a new link to" ${BASE_DIR}/${QT_PATH}/clang_64/bin/qmake
+if ln -s ${BASE_DIR}/${QT_PATH}/clang_64/bin/qmake /usr/bin/qmake
 then
 	echo Switched to Version
 	qmake -v
