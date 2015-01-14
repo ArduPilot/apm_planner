249bc8eaad592457f040a9ea135869c1520e2c34
Andre Kjellstrup <andre.kjellstrup@gmail.com>
2015-01-14 18:28:46 +0100

    Plan View: tooltip no longer adds MSL to any altitude.
    The WP tooltip said that any altitude were an MSL altitude, regardless
    of the wp being relative to home_alt.

--- libs/opmapcontrol/src/mapwidget/waypointitem.cpp
+++ libs/opmapcontrol/src/mapwidget/waypointitem.cpp
@@ -330,7 +330,7 @@ namespace mapcontrol
     void WayPointItem::RefreshToolTip()
     {
         QString coord_str = QString::number(coord.Lat(), 'f', 6) + "   " + QString::number(coord.Lng(), 'f', 6);
-        setToolTip(QString("WayPoint Number: %1\nDescription: %2\nCoordinate: %4\nAltitude: %5 m (MSL)\nHeading: %6 deg").arg(QString::number(WayPointItem::number)).arg(description).arg(coord_str).arg(QString::number(altitude)).arg(QString::number(heading)));
+        setToolTip(QString("WayPoint Number: %1\nDescription: %2\nCoordinate: %4\nAltitude: %5 m\nHeading: %6 deg").arg(QString::number(WayPointItem::number)).arg(description).arg(coord_str).arg(QString::number(altitude)).arg(QString::number(heading)));
     }
 
     int WayPointItem::snumber=0;
