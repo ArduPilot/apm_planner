7dd20725295906c237f18d74cb293bfbf2f34eb1
Bill Bonney <billbonney@communistech.com>
2015-02-11 09:25:05 -0800

    scripts: Update to TeamCity and ‘stable’ version

--- scripts/upload_osx.sh
+++ scripts/upload_osx.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#!/bin/sh
+# Move and Upload
 COMMIT_SHA=$(git describe --dirty)
-TARGET_DIR=/Users/Shared/Jenkins/Home/data/APMPlanner
-WEBSITE_USER=
+TARGET_DIR=/Users/admin/dev_src/APMPlanner
+WEBSITE_USER=TeamCityAdmin
 
-mv apm_planner2_$BUILD_ID\_osx.dmg ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg 
-cp ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg ${TARGET_DIR}/apm_planner2_latest_osx.dmg
-rsync -avh --password-file=rsyncpass ${TARGET_DIR} ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/
+if [ ! -d ${TARGET_DIR} ]; then
+  mkdir -vp ${TARGET_DIR}/
+fi
+mv -v apm_planner2_$BUILD_NUMBER\_osx.dmg ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg
+rsync -avh --password-file=../../../dev_src/rsyncpass ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/
