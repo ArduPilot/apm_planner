7afe01bb419337e3ccb84b7215ea11c90cd6acc6
Bill Bonney <billbonney@communistech.com>
2015-02-16 14:23:17 -0800

    scripts: update upload_osx build server script

--- scripts/upload_stable_osx.sh
+++ scripts/upload_stable_osx.sh
@@ -3,7 +3,7 @@ COMMIT_SHA=$(git describe --dirty)
 TARGET_DIR=/Users/admin/dev_src/APMPlanner
 WEBSITE_USER=TeamCityAdmin
 
-mv apm_planner2_$BUILD_ID\_osx.dmg ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg
+mv apm_planner2_$BUILD_NUMBER\_osx.dmg ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg
 cp ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg ${TARGET_DIR}/apm_planner2_latest_osx.dmg
 rsync -avh --password-file=../../../data/rsyncpass ${TARGET_DIR}/apm_planner_${COMMIT_SHA}_osx.dmg  ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/
 rsync -avh --password-file=../../../data/rsyncpass ${TARGET_DIR}/apm_planner2_latest_osx.dmg  ${WEBSITE_USER}@firmware.diydrones.com::APMPlanner/
