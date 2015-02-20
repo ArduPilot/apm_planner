2475ee2113c207d0e850db97258b1df638d5cc3a
Andrew Harrison <Harrisonheli@gmail.com>
2015-02-20 17:32:50 +1100

    Set the member variables to 0. Prevents random values crashing PrimaryFlightDisplay.qml

--- src/comm/AbsPositionOverview.cc
+++ src/comm/AbsPositionOverview.cc
@@ -3,6 +3,23 @@
 AbsPositionOverview::AbsPositionOverview(QObject *parent) :
     QObject(parent)
 {
+    this->m_alt = 0;
+    this->m_cog = 0;
+    this->m_eph = 0;
+    this->m_epv = 0;
+    this->m_fixType = 0;
+    this->m_hdg = 0;
+    this->m_lat = 0;
+    this->m_lon = 0;
+    this->m_relativeAlt = 0;
+    this->m_satellitesVisible = 0;
+    this->m_timeBootMs = 0;
+    this->m_timeUsec = 0;
+    this->m_vel = 0;
+    this->m_vx = 0;
+    this->m_vy = 0;
+    this->m_vz = 0;
+
 }
 
 AbsPositionOverview::~AbsPositionOverview()
