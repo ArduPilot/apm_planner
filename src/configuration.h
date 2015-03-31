47bd731d16cbe8892200e553d364ebe71a9d30b6
Michael Carpenter <malcom2073@gmail.com>
2015-03-31 12:08:02 -0400

    Version change for 2.0.17 release

--- src/configuration.h
+++ src/configuration.h
@@ -15,7 +15,7 @@
 #define WITH_TEXT_TO_SPEECH 1
 
 #define QGC_APPLICATION_NAME "APM Planner"
-#define QGC_APPLICATION_VERSION "v2.0.16"
+#define QGC_APPLICATION_VERSION "v2.0.17"
 #define APP_DATA_DIRECTORY "/apmplanner2"
 #define LOG_DIRECTORY "/dataflashLogs"
 #define PARAMETER_DIRECTORY "/parameters"
@@ -50,7 +50,7 @@ namespace QGC
 {
 const QString APPNAME = "APMPLANNER2";
 const QString COMPANYNAME = "DIYDRONES";
-const int APPLICATIONVERSION = 2016; // 2.0.16 [TODO] we should deprecate this version definition
+const int APPLICATIONVERSION = 2017; // 2.0.17 [TODO] we should deprecate this version definition
 
     inline void close(){
         GlobalObject* global = GlobalObject::sharedInstance();
