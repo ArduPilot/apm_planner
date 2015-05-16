92ff19da4e12f33360561897e9e757b1f4ee1b7f
Bill Bonney <billbonney@communistech.com>
2015-05-15 22:33:21 -0700

    Maps Widget: Update Google API Version
    And remove HTTPS calls

--- libs/opmapcontrol/src/core/opmaps.cpp
+++ libs/opmapcontrol/src/core/opmaps.cpp
@@ -221,6 +221,9 @@ namespace core {
                 tT.stop();
                 if( (reply->error()!=QNetworkReply::NoError))
                 {
+#ifdef DEBUG_GMAPS
+                    qDebug() << " Download Tile Network error: " << reply->errorString();
+#endif
                     errorvars.lock();
                     ++diag.networkerrors;
                     errorvars.unlock();
--- libs/opmapcontrol/src/core/providerstrings.cpp
+++ libs/opmapcontrol/src/core/providerstrings.cpp
@@ -42,10 +42,10 @@ namespace core {
 //        VersionGoogleLabels = "h@132";
 //        VersionGoogleTerrain = "t@125,r@132";
         // Google version strings
-        VersionGoogleMap = "m@284";
-        VersionGoogleSatellite = "163";
-        VersionGoogleLabels = "h@284";
-        VersionGoogleTerrain = "t@132,r@284";
+        VersionGoogleMap = "m@301";
+        VersionGoogleSatellite = "171";
+        VersionGoogleLabels = "h@301";
+        VersionGoogleTerrain = "t@132,r@301";
         SecGoogleWord = "Galileo";
 
         // Google (China) version strings
