ca85668deebcf1f0a3860aafda7a5f4ea0e7ee2f
Markus Reitboeck <mrpilot@gmx.net>
2015-08-01 17:19:57 +0200

    VersionComparator: added documentation

--- src/versioncomparator.h
+++ src/versioncomparator.h
@@ -3,10 +3,26 @@
 
 #include <QString>
 
+/**
+ * @brief Compares two version strings.
+ *
+ * This class can be used to compare two version strings.
+ * The strings need to contain a version number in the follwing format:
+ *      MAJOR.MINOR[.BUILD][-RELEASE_CANDIDATE]
+ * e.g.     2.0.18-rc2 or 2.1
+ *
+ **/
 class VersionComparator
 {
 public:
 
+    /**
+     * @brief compares two version strings
+     * @param newVersion: this version is checked if it is newer,
+     *                    it is probably fetched from the internet
+     * @param currentVersion: this is the current version installed on the system
+     * @return returns true if newVersion is newer than currentVersion
+     */
     static bool isVersionNewer(const QString& newVersion,
                                const QString& currentVersion);
 
