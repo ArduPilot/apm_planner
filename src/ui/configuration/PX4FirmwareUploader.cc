4dcd8d72acc871ed9ccb68e76709c4b7e38bd870
Bill Bonney <billbonney@communistech.com>
2015-10-11 14:27:19 -0700

    Serial Ports: Fix Qt5.5 reverting serial port naming to pre Qt5.4.1

--- src/ui/configuration/PX4FirmwareUploader.cc
+++ src/ui/configuration/PX4FirmwareUploader.cc
@@ -286,7 +286,7 @@ void PX4FirmwareUploader::kickOffTriggered()
     m_port = new QSerialPort();
     connect(m_port,SIGNAL(readyRead()),this,SLOT(portReadyRead()));
 
-#ifdef Q_OS_MACX
+#if defined(Q_OS_MACX) && ((QT_VERSION == 0x050402)||(QT_VERSION == 0x0500401))
     // temp fix Qt5.4.1 issue on OSX
     // http://code.qt.io/cgit/qt/qtserialport.git/commit/?id=687dfa9312c1ef4894c32a1966b8ac968110b71e
     m_port->setPortName("/dev/cu." + m_portToUse);
--- src/ui/configuration/TerminalConsole.cc
+++ src/ui/configuration/TerminalConsole.cc
@@ -200,7 +200,7 @@ void TerminalConsole::logsButtonClicked() {
 
 void TerminalConsole::openSerialPort(const SerialSettings &settings)
 {
-#ifdef Q_OS_MACX
+#if defined(Q_OS_MACX) && ((QT_VERSION == 0x050402)||(QT_VERSION == 0x0500401))
     // temp fix Qt5.4.1 issue on OSX
     // http://code.qt.io/cgit/qt/qtserialport.git/commit/?id=687dfa9312c1ef4894c32a1966b8ac968110b71e
     m_serial->setPortName("/dev/cu." + settings.name);
