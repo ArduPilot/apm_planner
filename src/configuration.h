68485d75c8edd6f6231c264cf78e5462a71ee7cb
Bill Bonney <billbonney@communistech.com>
2016-01-31 21:53:25 -0800

    Build: Update to 2.0.19-rc5

--- src/configuration.h
+++ src/configuration.h
@@ -15,7 +15,7 @@
 #define WITH_TEXT_TO_SPEECH 1
 
 #define QGC_APPLICATION_NAME "APM Planner"
-#define QGC_APPLICATION_VERSION "v2.0.19-rc4"
+#define QGC_APPLICATION_VERSION "v2.0.19-rc5"
 #define APP_DATA_DIRECTORY "/apmplanner2"
 #define LOG_DIRECTORY "/dataflashLogs"
 #define PARAMETER_DIRECTORY "/parameters"
