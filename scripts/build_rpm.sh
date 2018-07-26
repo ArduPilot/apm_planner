6041b7184e8d97639313f6fbe085d7636d00a869
Александр Фролов <froller@froller.net>
2018-07-26 12:16:01 +0300

    Fedora 26 build scripts and spec files

--- scripts/build_rpm.sh
+++ scripts/build_rpm.sh
@@ -5,5 +5,5 @@ then
     SPEC_FILE="${HOME}/rpmbuild/SPECS/apmplanner2.spec"
 fi
 
-rpmbuild -ba $SPEC_FILE --target=i686
+#rpmbuild -ba $SPEC_FILE --target=i686
 rpmbuild -ba $SPEC_FILE --target=x86_64
