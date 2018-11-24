4c0826c44c7ce59e3735f430eb74135085e03f80
Bill Bonney <bill@communistech.com>
2018-11-24 09:20:03 -0800

    build: Update to 2.0.27-rc1

--- src/configuration.h
+++ src/configuration.h
@@ -15,7 +15,7 @@
 #define WITH_TEXT_TO_SPEECH 1
 
 #define QGC_APPLICATION_NAME "APM Planner"
-#define QGC_APPLICATION_VERSION "v2.0.26-rc2"
+#define QGC_APPLICATION_VERSION "v2.0.27-rc1"
 #define APP_DATA_DIRECTORY "/apmplanner2"
 #define LOG_DIRECTORY "/dataflashLogs"
 #define PARAMETER_DIRECTORY "/parameters"
@@ -60,7 +60,7 @@ namespace QGC
 {
 const QString APPNAME = "APMPLANNER2";
 const QString COMPANYNAME = "ARDUPILOT";
-const int APPLICATIONVERSION = 2026; // 2.0.26 [TODO] we should deprecate this version definition
+const int APPLICATIONVERSION = 2027; // 2.0.27 [TODO] we should deprecate this version definition
 
     inline void close(){
         GlobalObject* global = GlobalObject::sharedInstance();
