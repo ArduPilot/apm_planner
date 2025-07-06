6065527da5476465a77a8115f0290ac9cbb7b384
Bill Bonney <bill@communistech.com>
2025-07-06 13:29:40 +1200

    Fixes: Remove 310 warnings due to unused variable

--- libs/mavlink/include/mavlink/v2.0/mavlink_helpers.h
+++ libs/mavlink/include/mavlink/v2.0/mavlink_helpers.h
@@ -585,8 +585,6 @@ MAVLINK_HELPER uint8_t mavlink_frame_char_buffer(mavlink_message_t* rxmsg,
                                                  mavlink_message_t* r_message, 
                                                  mavlink_status_t* r_mavlink_status)
 {
-	int bufferIndex = 0;
-
 	status->msg_received = MAVLINK_FRAMING_INCOMPLETE;
 
 	switch (status->parse_state)
@@ -816,7 +814,6 @@ MAVLINK_HELPER uint8_t mavlink_frame_char_buffer(mavlink_message_t* rxmsg,
 		break;
 	}
 
-	bufferIndex++;
 	// If a message has been successfully decoded, check index
 	if (status->msg_received == MAVLINK_FRAMING_OK)
 	{
